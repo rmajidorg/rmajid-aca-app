2099123b25c3043bd9ff3f51e4e9c6dff1bb7d50
rmajid2 <36069828+rmajid2@users.noreply.github.com>
2023-06-11 00:53:26 +0200

    Update setup-webhook.js

--- src/setup-webhook.js
+++ src/setup-webhook.js
@@ -1,11 +1,12 @@
 const Script = async ({github, context, core}, repoName) => {
+  const {WEBHOOK_SELF_HOSTED_RUNNER} = process.env;
   await github.rest.repos.createWebhook({
         owner: 'rmajid-aca-app',
         repo: repoName,
         config: {
           url: 'https://c32386e4-7e51-4ad8-8959-c2a6ba5310ea.mock.pstmn.io',
           content_type: 'json',
-          secret: 'kab8Q~~2a9H3JtWLS5glyh-eq6xHM2Od2RG.fdvY'
+          secret: `${WEBHOOK_SELF_HOSTED_RUNNER}`
         },
         events: ["workflow_job"],
         active: true
