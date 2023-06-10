b32dfd1ed5570eee7df52ae2146d5d4146cc6327
rmajid2 <36069828+rmajid2@users.noreply.github.com>
2023-06-11 01:41:53 +0200

    Update setup-webhook.js

--- src/setup-webhook.js
+++ src/setup-webhook.js
@@ -2,6 +2,8 @@ const Script = async ({github, context, core}, repoName) => {
   const {WEBHOOK_SELF_HOSTED_RUNNER} = process.env;
   const webhookUrl = 'https://c32386e4-7e51-4ad8-8959-c2a6ba5310ea.mock.pstmn.io';
   
+  
+  console.log('Get List of all Webhook');
   const webhooksOpts = github.rest.repos.listWebhooks({
         owner: 'rmajidorg',
         repo: repoName
@@ -9,8 +11,10 @@ const Script = async ({github, context, core}, repoName) => {
   
   const webhooks = await github.paginate(webhooksOpts)
 
+  
+    console.log('Check if the self hosted runner already exist');
   for (const webhook of webhooks) {
-    console.log(webhook);
+    console.log(`Iteration : ${webhook}`);
     if (webhook.url === webhookUrl) {
       console.log('Webhook Already created');
       await github.rest.repos.updateWebhook({
